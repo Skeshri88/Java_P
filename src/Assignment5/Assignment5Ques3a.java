d4c23d0096b02711799c15242eb3b4b5bd5ae2b6
Admin <Admin@DESKTOP-PJTEI91>
2020-04-16 11:07:34 +0530

    dd

--- src/Assignment5/Assignment5Ques3a.java
+++ src/Assignment5/Assignment5Ques3a.java
@@ -2,5 +2,29 @@ package Assignment5;
 
 public class Assignment5Ques3a 
 {
+	
+	
+	 public int balance = 2500;
+	    public boolean deposit(int amount){
+	        if(amount<0){
+	            return false;
+	        }
+	        this.balance +=amount;
+	        return true;
+	    }
 
-}
+	    public boolean withdraw(int amount){
+	        if(this.balance-amount>0){
+	            this.balance -= amount;
+	            return true;
+	        }
+	        else{
+	            return false;
+	        }
+	        
+	    }
+
+	
+	}
+
+
